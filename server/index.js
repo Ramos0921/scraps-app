c449aad2127c62e84b6fe66208b6ac293356ac85
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2020-07-25 15:51:42 -0700

    added routes/schema/data to database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors')
 var morgan = require('morgan')
-
-// var items = require('../database-mongo');
+var router = require('../router/router.js');
+var db = require('../database-mongo');
 
 var app = express();
 
@@ -13,20 +14,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-
-
-app.get('/items', function (req, res) {
-  res.send('works')
-  // items.selectAll(function(err, data) {
-  //   if(err) {
-  //     res.sendStatus(500);
-  //   } else {
-  //     res.json(data);
-  //   }
-  // });
-});
-
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+app.use('/',router);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log('Connected to Port: '+PORT);
 });
 
